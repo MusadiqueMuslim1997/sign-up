fe5ef1ea79cbc5d9ad5c8bfa04edb8ae51ba0b33
MusadiqueMuslim1997 <137433475+MusadiqueMuslim1997@users.noreply.github.com>
2024-03-16 23:36:44 +0500

    add

--- app.js
+++ app.js
@@ -3,15 +3,20 @@
 var signUp = document.getElementById("signup");
 var signIn = document.getElementById("signin");
 
-function sign(){
+function sign() {
     var password = document.getElementById("password").value;
     var repeat = document.getElementById("repeat").value;
-var fn = document.getElementById("fn").value;
-var ln = document.getElementById("ln").value;
-var em = document.getElementById("em").value;
-   if(password == repeat && password != "" && repeat != "" &&  fn != "" && ln != "" && em != ""){
-    signUp.style.display = "none";
-    signIn.style.display = "flex";
-   }
-  
+    var fn = document.getElementById("fn").value;
+    var ln = document.getElementById("ln").value;
+    var em = document.getElementById("em").value;
+    if (password == repeat && password != "" && repeat != "" && fn != "" && ln != "" && em != "") {
+        signUp.style.display = "none";
+        signIn.style.display = "flex";
+    }
+    else if (password == "" || repeat == "" || fn == "" || ln == "" || em == "") {
+        alert("Please Fill Input")
+    }
+    else if (password != repeat) {
+        alert("Your repeat password is wrong")
+    }
 }
