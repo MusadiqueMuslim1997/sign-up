625ab5f6730658faeb23636fddf2cfe495700b52
MusadiqueMuslim1997 <137433475+MusadiqueMuslim1997@users.noreply.github.com>
2024-03-19 15:41:54 +0500

    add

--- app.js
+++ app.js
@@ -16,12 +16,12 @@ function sign() {
     var password = document.getElementById("password").value;
     var repeat = document.getElementById("repeat").value;
     var fn = document.getElementById("fn").value;
-    var ln = document.getElementById("ln").value;
+   
     var em = document.getElementById("em").value;
-    if (password == repeat && password != "" && repeat != "" && fn != "" && ln != "" && em != "") {
+    if (password == repeat && password != "" && repeat != "" && fn != "" &&  em != "") {
        window.location.assign("login.html");
     }
-    else if (password == "" || repeat == "" || fn == "" || ln == "" || em == "") {
+    else if (password == "" || repeat == "" || fn == ""  || em == "") {
         alert("Please Fill Input")
     }
     else if (password != repeat) {
