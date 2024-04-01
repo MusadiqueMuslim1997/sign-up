deacab81d03547f7cd3fd6ef80a0f3a686cd8023
MusadiqueMuslim1997 <137433475+MusadiqueMuslim1997@users.noreply.github.com>
2024-04-01 15:32:34 +0500

    add

--- app.js
+++ app.js
@@ -58,7 +58,7 @@ var htmlQuiz = [
     },
 
     {
-        que: '2. What is the abbreviation of HTML',
+        que: '2. What is the abbreviation of HTML?',
         opt1: 'Hyper Text Markup Language',
         opt2: 'Hyper Info Markup Language',
         opt3: 'Hyper Tend Markup Language',
@@ -76,7 +76,64 @@ var htmlQuiz = [
         opt4: 'Important',
         ans: 'Important'
     },
+    {
+        que: '4. In which of the following ways can we create strings in JavaScript?',
+        opt1: "'A string'",
+        opt2: '"A string"',
+        opt3: '`A string`',
+        opt4: 'All of the above',
+        ans: 'All of the above'
+    },
 
+
+    {
+        que: '5. How to convert a string str to lowercase characters?',
+        opt1: 'str.lower()',
+        opt2: 'str.toLower()',
+        opt3: 'str.toLowerCase()',
+        opt4: 'None',
+        ans: 'str.toLowerCase()'
+    },
+    {
+        que: '6. var str = "I love cats!";  console.log(str.indexOf("cat"));',
+        opt1: '6',
+        opt2: '7',
+        opt3: '-1',
+        opt4: '8',
+        ans: '7'
+    },
+    {
+        que: '7. How can we extract "Pizza" from the string str="Pizza Lover"?',
+        opt1: 'str.slice(0, 4)',
+        opt2: 'str.slice(0, 5)',
+        opt3: 'str.slice(4, 0)',
+        opt4: 'str.slice(5, 0)',
+        ans: 'str.slice(0, 5)'
+    },
+    {
+        que: '8. What does str.charAt(1) return for the string str = "Hello"?',
+        opt1: '"H"',
+        opt2: '"e"',
+        opt3: '"l"',
+        opt4: 'undefined',
+        ans: '"e"'
+    },
+    {
+        que: '9. Suppose that str is equal to the string "Mango Apple  Orange". What will str.split() return?',
+        opt1: '["Mango", "Apple", "Orange"]',
+        opt2: '["Mango", "Apple", "", "Orange"]',
+        opt3: '["Mango Apple  Orange"]',
+        opt4: 'All of the above',
+        ans: '["Mango", "Apple", "Orange"]'
+    },
+    {
+        que: '10. The external JavaScript file must contain the <script> tag.',
+        opt1: 'True',
+        opt2: 'False',
+        opt3: 'All of the above',
+        opt4: 'None of the above',
+        ans: 'False'
+    },
 ]
 
 
